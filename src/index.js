1b83b9e49c2f37a6726468d74eefc24978fdd1ad
Koras02 <wjdgh0727@gmail.com>
2020-11-05 22:19:18 +0900

    2:35:00

--- src/index.js
+++ src/index.js
@@ -4,26 +4,45 @@ import ReactDom from 'react-dom';
 // CSS
 import './index.css';
 // setup vars
-const author = 'Amelia Hepworth';
+const firstBook = {
+  img:'https://images-na.ssl-images-amazon.com/images/I/51rwXABgAYL._SX258_BO1,204,203,200_.jpg',
+  title:'Trick or Treast, Baby Shark!: Doo Doo Doo Doo Doo Doo (A Baby Shark Boosks)',
+  author: 'Amelia Hepworth'
+}
+const secondBook = {
+  img:'https://images-na.ssl-images-amazon.com/images/I/41rbthdtokL._SX390_BO1,204,203,200_.jpg',
+  title:'Greenlights Hardcover – October 20, 2020',
+  author: 'Matthew McConaughey'
+}
+
 function BookList() {
    return (
    <section className='booklist'>
-     <Book />
+     <Book 
+      img={firstBook.img} 
+      title={firstBook.title} 
+      author={firstBook.author} 
+     >
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad sunt necessitatibus accusantium quibusdam aperiam possimus atque, 
+          pariatur qui sapiente eius.</p>
+     </Book>
+     <Book 
+      img={secondBook.img} 
+      title={secondBook.title}
+      author={secondBook.author}/>
    </section>
   );
 }
-const Book = () =>{
-  const title = "Trick or Treast, Baby Shark!: Doo Doo Doo Doo Doo Doo (A Baby Shark Boosks)"
+
+const Book = (props) =>{
+  const { img, title, author } = props;
+  console.log(props);
   return (
-    <article className="book">
-    <img 
-    src='https://images-na.ssl-images-amazon.com/images/I/51rwXABgAYL._SX258_BO1,204,203,200_.jpg'
-   alt=''
-   />
+    <article className='book'> 
+    <img src={img} alt='' />
    <h1>{title}</h1>
-   <h4>{author.toUpperCase()}</h4>
-   {/* <p>{let x= 6}</p> */}
-   <p>{6 * 6}</p>
+   <h4>{author}</h4>
+   {props.children}
     </article>
   );
 };
